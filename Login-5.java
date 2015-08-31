aa62fdc77acf6f16c419071b5dd240e4fb562203
Ricardo Olivo <ricardo_olivo@hotmail.com>
2015-08-31 01:33:40 -0300

    Login
    Login

---
+++ Login-5.java
@@ -0,0 +1,37 @@
+import java.util.concurrent.TimeUnit;
+import java.util.Date;
+import java.io.File;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+import static org.openqa.selenium.OutputType.*;
+
+public class Login-5 {
+    public static void main(String[] args) throws Exception {
+        FirefoxDriver wd;
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://69.164.203.63:8080/login/");
+        wd.findElement(By.id("id_usuario")).click();
+        wd.findElement(By.id("id_usuario")).clear();
+        wd.findElement(By.id("id_usuario")).sendKeys("RFOLIVO@GMAIL.COM");
+        wd.findElement(By.id("id_senha")).click();
+        wd.findElement(By.id("id_senha")).clear();
+        wd.findElement(By.id("id_senha")).sendKeys("ricardo123");
+        wd.findElement(By.xpath("//form[@class='form-login']//button[.='ENTRAR']")).click();
+        if (!(wd.findElements(By.id("img_colaborador_logado")).size() != 0)) {
+            System.out.println("verifyElementPresent failed");
+        }
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ README.txt
@@ -0,0 +1,17 @@
+Projeto LOGIN
+
+Esse projeto foi desenvolvido através da ferramenta Selenium Builder.
+
+Passos para executar o teste:
+
+1 - Instalar o plugin - Selenium Builder no Firefox versão 39
+2 - Abrir a ferramenta e selecionar a opção File > Open a script or Suite.
+3 - Importar o arquivo Suite-Login.json
+4 - Após incluir o projeto, executar na opção: Run > Run suite locally.
+5 - Todos os scritps serão executados na ordem correta.
+6 - Os detalhes dos passos de cada script está no documento Roteiro de testes.xls
+
+Qualquer dúvida estou a disposição - rfolivo@gmail.com
+
+Att.
+Ricardo Olivo
